7ed61f9fdbacd27a5afa6e940fb966ab42632cbf
Plamena998 <plami_d@mail.bg>
2025-11-18 23:06:35 +0200

    add like button, js, css

--- WebApp/wwwroot/js/car.js
+++ WebApp/wwwroot/js/car.js
@@ -3,7 +3,7 @@
         var carId = $(this).data("car-id");
         console.log("Button clicked, car id:", carId);
 
-        // Премахваме стария модал, ако съществува
+        // Премахва стария модал, ако съществува
         var oldModal = document.getElementById("carModal");
         if (oldModal) {
             var oldInstance = bootstrap.Modal.getInstance(oldModal);
@@ -31,3 +31,77 @@
         });
     });
 });
+
+
+
+//helper toast (Bootstrap alert-like) 
+function showSuccessMessage(message) {
+
+    //Създава alert елемент (Bootstrap alert класове)
+    var $alert = $(
+        '<div class="alert alert-success alert-dismissible fade show custom-toast" role="alert">' +
+        message +
+        '</div>'
+    );
+
+    $("body").append($alert);
+
+    //Auto close след 2 секунди
+    setTimeout(function () {
+        $alert.alert('close');
+    }, 2000);
+
+    //Премахва напълно от DOM когато е скрит
+    $alert.on('closed.bs.alert', function () {
+        $alert.remove();
+    });
+}
+
+//Toggle like (delegated binding)
+$(document).on("click", ".like-btn", function (e) {
+    e.preventDefault();
+
+    var $btn = $(this);
+    var carId = $btn.data("car-id");
+
+    //Toggle class
+    $btn.toggleClass("liked");
+
+    //Toggle aria-pressed for accessibility
+    var pressed = $btn.hasClass("liked");
+    $btn.attr("aria-pressed", pressed ? "true" : "false");
+
+    //Toggle icon classes (outline <-> fill)
+    var $icon = $btn.find("i");
+    if ($btn.hasClass("liked")) {
+        $icon.removeClass("bi-hand-thumbs-up").addClass("bi-hand-thumbs-up-fill");
+        showSuccessMessage("Колата е добавена към любими!");
+    } else {
+        $icon.removeClass("bi-hand-thumbs-up-fill").addClass("bi-hand-thumbs-up");
+        showSuccessMessage("Колата е премахната от любими.");
+    }
+
+    try {
+        var key = "favoriteCars";
+        var favorites = JSON.parse(localStorage.getItem(key) || "[]");
+        if ($btn.hasClass("liked")) {
+            // добавя ако не съществува
+            if (favorites.indexOf(carId) === -1) favorites.push(carId);
+        } else {
+            // премахва
+            favorites = favorites.filter(function (id) { return id != carId; });
+        }
+        localStorage.setItem(key, JSON.stringify(favorites));
+    } catch (err) {
+        //ignore localStorage errors in private mode
+        console.warn("localStorage error:", err);
+    }
+
+    //AJAX запис в бекенда
+    /*
+    $.post("/Favorites/Toggle", { id: carId })
+        .done(function(resp) { /* обработи резултат ако трябва *\/ })
+        .fail(function() { showSuccessMessage("Възникна грешка при добавяне."); });
+    */
+});
+
